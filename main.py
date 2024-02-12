688a153033a959fb142e7a67c475364bf40573a9
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-02-12 10:50:44 -0800

    implement oop

--- main.py
+++ main.py
@@ -2,44 +2,43 @@ import json
 from uuid import uuid4
 
 
-class Task_Id(object):
+class TaskId():
     def __init__(self):
         self.uid = uuid4().hex
 
 
-def load_tasks():
-    # need try except bc this will error if file does not exists
-    try:
-        with open('tasks.json', 'r') as file:
-            tasks = json.load(file)
-    except FileNotFoundError:
-        tasks = []
-    return tasks
+class TaskController():
+    def load_tasks(self):
+        # need try/except bc this will error if file does not exists
+        try:
+            with open('tasks.json', 'r') as file:
+                tasks = json.load(file)
+        except FileNotFoundError:
+            tasks = []
+        return tasks
 
+    def save_tasks(self, tasks):
+        with open('tasks.json', 'w') as file:
+            json.dump(tasks, file, indent=4)
 
-def save_tasks(tasks):
-    with open('tasks.json', 'w') as file:
-        json.dump(tasks, file, indent=4)
+    def add_task(self, tasks, title, description):
+        task_id = TaskId()
+        new_task = {'id': task_id.uid, 'title': title,
+                    'description': description}
+        tasks.append(new_task)
 
+    def view_tasks(self, tasks):
+        print('Current Tasks: ', tasks)
 
-def add_task(tasks, title, description):
-    task_id = Task_Id()
-    new_task = {'id': task_id.uid, 'title': title, 'description': description}
-    tasks.append(new_task)
-
-
-def view_tasks(tasks):
-    print('Current Tasks: ', tasks)
-
-
-def delete_task(tasks, task_id):
-    for index, task in enumerate(tasks):
-        if task['id'] == str(task_id):
-            tasks.pop(index)
+    def delete_task(self, tasks, task_id):
+        for index, task in enumerate(tasks):
+            if task['id'] == str(task_id):
+                tasks.pop(index)
 
 
 def main():
-    tasks = load_tasks()
+    task = TaskController()
+    tasks = task.load_tasks()
     try:
         while True:
             print('1: Add')
@@ -52,14 +51,14 @@ def main():
                 print('Add Task')
                 title = input('Enter title: ')
                 description = input('Enter description: ')
-                add_task(tasks, title, description)
+                task.add_task(tasks, title, description)
             elif choice == '2':
-                view_tasks(tasks)
+                task.view_tasks(tasks)
             elif choice == '3':
                 task_id = input('Enter task ID to delete: ')
-                delete_task(tasks, task_id)
+                task.delete_task(tasks, task_id)
             elif choice == '4':
-                save_tasks(tasks)
+                task.save_tasks(tasks)
                 break
     except KeyboardInterrupt:
         exit()

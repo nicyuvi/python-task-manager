d6743ee71f78d665ea63c24cd9ad89520c01412d
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-02-15 11:26:51 -0800

    edit task feature. add utils and contants module

---
+++ constants.py
@@ -0,0 +1,4 @@
+prompt_title = 'Enter a title: '
+prompt_desc = 'Enter a description: '
+prompt_delete = 'Enter task ID to delete: '
+prompt_edit = 'Enter task ID to edit: '
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
+from typing import Dict, List
 from uuid import uuid4
+from utils import input_with_prefill
+from contants import prompt_title, prompt_desc, prompt_edit, prompt_delete
 
 
 class TaskId():
@@ -8,6 +11,9 @@ class TaskId():
 
 
 class TaskController():
+    # def get_task(self, task_id):
+    #     #get task
+
     def load_tasks(self):
         # need try/except bc this will error if file does not exists
         try:
@@ -21,7 +27,7 @@ class TaskController():
         with open('tasks.json', 'w') as file:
             json.dump(tasks, file, indent=4)
 
-    def add_task(self, tasks, title, description):
+    def add_task(self, tasks: List[Dict[str, str]], title, description):
         task_id = TaskId()
         new_task = {'id': task_id.uid, 'title': title,
                     'description': description}
@@ -35,6 +41,15 @@ class TaskController():
             if task['id'] == str(task_id):
                 tasks.pop(index)
 
+    def edit_task(self, tasks: List[Dict[str, str]],  task_id: str):
+        for task in tasks:
+            if task['id'] == str(task_id):
+                task['title'] = input_with_prefill(
+                    prompt_title, task['title'])
+                task['description'] = input_with_prefill(prompt_desc,
+                                                         task['description'])
+                break
+
 
 def main():
     task = TaskController()
@@ -44,20 +59,24 @@ def main():
             print('1: Add')
             print('2: View')
             print('3: Delete Task')
-            print('4: Save and Exit')
+            print('4: Edit Task')
+            print('5: Save and Exit')
             choice = input('Please select an option: ')
 
             if choice == '1':
                 print('Add Task')
-                title = input('Enter title: ')
-                description = input('Enter description: ')
+                title = input(prompt_title)
+                description = input(prompt_desc)
                 task.add_task(tasks, title, description)
             elif choice == '2':
                 task.view_tasks(tasks)
             elif choice == '3':
-                task_id = input('Enter task ID to delete: ')
+                task_id = input(prompt_delete)
                 task.delete_task(tasks, task_id)
             elif choice == '4':
+                task_id = input(prompt_edit)
+                task.edit_task(tasks, task_id)
+            elif choice == '5':
                 task.save_tasks(tasks)
                 break
     except KeyboardInterrupt:
